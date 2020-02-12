96a5039ea586e24859e7b8fd56dbb4b42cb63906
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-02-12 09:22:39 -0300

    Initial commit

---
+++ bnf.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string expression;
+    cout << "Enter the expression(without spaces): ";
+    cin >> expression;
+    cout << expression << endl;
+    return 0;
+}
